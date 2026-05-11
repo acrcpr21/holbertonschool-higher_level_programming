34c2bd97a710731c6eda7866d70680dd68a17ece
acrcpr21 <acrcpr@gmail.com>
2026-05-11 14:31:25 -0400

    Sliced a string to extract first 3, last 2, and middle characters

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+char_1 = word[0:3]
+char_2 = word[-2:]
+middle_char = word[1:-1]
+print(f"First 3 letters: {char_1}")
+print(f"Last 2 letters: {char_2}")
+print(f"Middle word: {middle_char}")

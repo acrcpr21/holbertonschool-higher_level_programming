9e425f0988c483ed0caa68986bac9fdcd8fe3093
acrcpr21 <acrcpr@gmail.com>
2026-05-11 14:32:06 -0400

    Defined arithmetic functions module with add, sub, mul, and div operations

---
+++ python-import_modules/calculation_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    print("{} + {} = {}".format(a, b, a + b))
+    return a + b
+
+def sub(a, b):
+    print("{} - {} = {}".format(a, b, a - b))
+    return a - b
+
+def mul(a, b):
+    print("{} * {} = {}".format(a, b, a * b))
+    return a * b
+
+def div(a, b):
+    print("{} / {} = {}".format(a, b, a / b))
+    return a / b
